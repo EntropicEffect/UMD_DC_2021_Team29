3ff1c472fea39ee4cb47c031b7c3cdbbd70c7fe0
Irfaan <irfaanj9@gmail.com>
2021-02-20 14:42:32 -0500

    changed lat/long data type to float

--- mapnode.py
+++ mapnode.py
@@ -1,7 +1,7 @@
 class MapNode():
-    def __init__(self, lattitude, longitude):
-        self.lat = str(lattitude)
-        self.long = str(longitude)
+    def __init__(self, latitude, longitude):
+        self.lat = float(latitude)
+        self.long = float(longitude)
     
     # You can now do print(node) instead of node.print()
     def __repr__(self):
