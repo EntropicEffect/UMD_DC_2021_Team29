881f5925d394f5cc906ea7848eeef9a17205eea2
Irfaan <irfaanj9@gmail.com>
2021-02-22 10:18:52 -0500

    added metro boundary as pickle file

---
+++ metro_boundary.py
@@ -0,0 +1,22 @@
+from point import Point
+from polygon import Polygon
+import os
+import pickle
+
+dir = os.path.dirname(__file__)
+filename = os.path.join(dir, 'metro_boundary')
+points = []
+with open(filename) as file:
+    for line in file.readlines():
+        p = line.split(', ')
+        points.append(Point(float(p[0]), float(p[1])))
+
+poly = Polygon(points)
+
+with open('metro_boundary.pickle', 'wb') as handle:
+    pickle.dump(poly, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('metro_boundary.pickle', 'rb') as handle:
+    metro_boundary = pickle.load(handle)
+    
+print(metro_boundary)
